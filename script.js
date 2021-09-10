0978bad857bf0013908c27fd7e67056b60df3c17
DivyanshuCoder <88090146+DivyanshuCoder@users.noreply.github.com>
2021-09-10 17:33:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function tell(){
+  window.alert("this is sun its the only sorce of light for our planet earth")
+}
+function tell2(){
+  window.alert("this is the Earth theplanet where we and many other animals and incets live togather and this is the the only perfet planet for human to live")
+}
